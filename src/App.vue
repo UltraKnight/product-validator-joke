7bded1574a3204fc58aaeb43670aa9d881e3d886
vanderlei <vanderlei.i.martins@outlook.com>
2025-12-24 15:02:10 +0000

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import Header from './components/validation/Header.vue';
 import ValidationSteps from './components/validation/steps/ValidationSteps.vue';
 </script>
 
 <template>
+  <Header />
   <ValidationSteps />
 </template>
---
+++ src/components/validation/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <header>
+      <h1>Registro e Validação</h1>
+      <p>Verifique a autenticidade do seu produto original</p>
+    </header>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+header {
+  width: 100vw;
+  margin-bottom: 10px;
+  background-color: #1e293b;
+  color: #ffffff;
+  padding: 10px;
+  text-align: center;
+}
+
+header h1 {
+  margin: 0;
+  font-size: 28px;
+}
+
+header p {
+  margin-top: 8px;
+  font-size: 16px;
+  opacity: 0.9;
+}
+</style>
